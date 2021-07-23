471169246c621dd5b8cf568046b38b8fe926337d
Shikha Suman <shikhasuman4112001@gmail.com>
2021-07-23 21:48:01 +0530

    Add Level-3 Security

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -20,8 +20,6 @@ const userSchema = new mongoose.Schema ({
   password: String
 });
 
-
-userSchema.plugin(encrypt, { secret: process.env.SECRETS,encryptedFields: ["password"] }); //add this plugin before creating the mongoose model
 
 const User = new mongoose.model("User",userSchema);
 
@@ -40,7 +38,7 @@ app.get("/register",(req,res)=>{
 app.post("/register",(req,res)=>{
   const newUser = new User({
     email:req.body.username,
-    password:req.body.password
+    password:md5(req.body.password)
   });
   newUser.save((err)=>{
     if(err){
@@ -53,7 +51,7 @@ app.post("/register",(req,res)=>{
 
 app.post("/login", function(req, res) {
   const username = req.body.username
-  const password = req.body.password
+  const password = md5(req.body.password)
 
   User.findOne({email: username}, function(err, foundUser){
     if(foundUser){
